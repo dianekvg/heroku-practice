7851ee23f0af137ff662985af95ce0b26774cab9
Diane VanGoethem <dianekvg@gmail.com>
2018-01-27 13:42:45 -0600

    update to 5

--- ChatterBot.py
+++ ChatterBot.py
@@ -6,6 +6,7 @@ import time
 is_prod = os.environ.get('IS_HEROKU', None)
 
 if is_prod:
+   
     #here goes all your heroku config
 
     consumer_key = os.environ.get('twitter_consumer_key', None)
@@ -39,13 +40,13 @@ def TweetOut(tweet_number):
         "Live, from Heroku! Can't stop. Won't stop. This is Tweet #%s!" %
         tweet_number)
 
-counter = 0
-for x in range(3):
-# Call the TweetQuotes function and specify the tweet number
-    TweetOut(counter)
+    counter = 0
+    for x in range(5):
+    # Call the TweetQuotes function and specify the tweet number
+        TweetOut(counter)
 
-    # Once tweeted, wait 60 seconds before doing anything else
-    time.sleep(60)
+        # Once tweeted, wait 60 seconds before doing anything else
+        time.sleep(60)
 
-    # Add 1 to the counter prior to re-running the loop
-    counter = counter + 1
+        # Add 1 to the counter prior to re-running the loop
+        counter = counter + 1
