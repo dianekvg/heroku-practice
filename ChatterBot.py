bcf83fc6507a91f8009422509c4cfdb7acf6e653
Diane VanGoethem <dianekvg@gmail.com>
2018-01-27 13:46:20 -0600

    update ChatterBot

--- ChatterBot.py
+++ ChatterBot.py
@@ -40,13 +40,13 @@ def TweetOut(tweet_number):
         "Live, from Heroku! Can't stop. Won't stop. This is Tweet #%s!" %
         tweet_number)
 
-    counter = 0
-    for x in range(5):
-    # Call the TweetQuotes function and specify the tweet number
-        TweetOut(counter)
+counter = 0
+for x in range(3):
+# Call the TweetQuotes function and specify the tweet number
+    TweetOut(counter)
 
-        # Once tweeted, wait 60 seconds before doing anything else
-        time.sleep(60)
+    # Once tweeted, wait 60 seconds before doing anything else
+    time.sleep(60)
 
-        # Add 1 to the counter prior to re-running the loop
-        counter = counter + 1
+    # Add 1 to the counter prior to re-running the loop
+    counter = counter + 1
